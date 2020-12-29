4f5997ae4b0f605f7cab7704a6f4312e56faa999
HaifaaNabeel <hauof53@gmail.com>
2020-12-30 01:17:26 +0300

    api for filter tenders by major , company and locstion

--- backend/app/Http/Controllers/Tender/TenderController.php
+++ backend/app/Http/Controllers/Tender/TenderController.php
@@ -53,10 +53,114 @@ class TenderController extends Controller
         return response()->json($collection,200);
       }
 
-    public function filterAllActiveTender()
-    {
-        return response()->json(tender::where('active','1')->orderByRaw('deadline - start_date DESC')->get(),200);
-    }
+      public function filterAllActiveTender(Request $request)
+      {
+          $id=$request->input('major_id');
+          $comp=$request->input('company');
+          $loc=$request->input('location');
+          $id_ar=explode(',', $id);
+          $comp_ar=explode(',', $comp);
+          $loc_ar=explode(',', $loc);
+          //////no filters 
+          if($id == '' && $comp == '' && $loc == '') 
+          {
+              $tender=tender::where('active','1')->where('deadline','>=',now())->where('start_date','<=',now())->orderByRaw('start_date DESC')->get();
+              return response()->json($tender,200);
+      
+          }
+          //////////filtter by major_id
+          if($id != '' && $comp == '' && $loc == '')
+          {
+              return response()->json(tender::where(function ($query) use ($id_ar) 
+              {
+                  foreach($id_ar as $va)
+                     $query->orwhere('major_id', '=', $va);
+                  })->where('active','1')->where('deadline','>=',now())->where('start_date','<=',now())->orderByRaw('start_date DESC')->get(),200);
+      
+          }
+                  //////////filtter by company name
+          if($id == '' && $comp != '' && $loc == '')
+          {
+              return response()->json(tender::where(function ($query) use ($comp_ar) 
+              {
+                  foreach($comp_ar as $va)
+                     $query->orwhere('company', '=', $va);
+                  })->where('active','1')->where('deadline','>=',now())->where('start_date','<=',now())->orderByRaw('start_date DESC')->get(),200);
+      
+          }
+          //////////filtter by location 
+          if($id == '' && $comp == '' && $loc != '')
+          {
+              return response()->json(tender::where(function ($query) use ($loc_ar) 
+              {
+                  foreach($loc_ar as $va)
+                     $query->orwhere('location', '=', $va);
+                  })->where('active','1')->where('deadline','>=',now())->where('start_date','<=',now())->orderByRaw('start_date DESC')->get(),200);
+      
+          }
+          //////////filtter by major_id and company name
+          if($id != '' && $comp != '' && $loc == '')
+          {
+              return response()->json(tender::where(function ($query) use ($id_ar) 
+              {
+                  foreach($id_ar as $va)
+                     $query->orwhere('major_id', '=', $va);
+                  })->where(function ($query) use ($comp_ar) 
+                  {
+                      foreach($comp_ar as $va)
+                         $query->orwhere('company', '=', $va);
+                      })->where('active','1')->where('deadline','>=',now())->where('start_date','<=',now())->orderByRaw('start_date DESC')->get(),200);
+      
+          }
+          //////////filtter by major_id and location
+          if($id != '' && $comp == '' && $loc != '')
+          {
+              return response()->json(tender::where(function ($query) use ($id_ar) 
+              {
+                  foreach($id_ar as $va)
+                     $query->orwhere('major_id', '=', $va);
+                  })->where(function ($query) use ($loc_ar) 
+                  {
+                      foreach($loc_ar as $va)
+                         $query->orwhere('location', '=', $va);
+                      })->where('active','1')->where('deadline','>=',now())->where('start_date','<=',now())->orderByRaw('start_date DESC')->get(),200);
+      
+          }
+          //////////filtter by company name and location
+          if($id == '' && $comp != '' && $loc != '')
+          {
+              return response()->json(tender::where(function ($query) use ($comp_ar) 
+              {
+                  foreach($comp_ar as $va)
+                     $query->orwhere('company', '=', $va);
+                  })->where(function ($query) use ($loc_ar) 
+                  {
+                      foreach($loc_ar as $va)
+                         $query->orwhere('location', '=', $va);
+                      })->where('active','1')->where('deadline','>=',now())->where('start_date','<=',now())->orderByRaw('start_date DESC')->get(),200);
+      
+          }
+          //////////filtter by major id , company name and location
+          if($id != '' && $comp != '' && $loc != '')
+          {
+              return response()->json(tender::where(function ($query) use ($id_ar) 
+              {
+                  foreach($id_ar as $va)
+                     $query->orwhere('major_id', '=', $va);
+                  })->where(function ($query) use ($comp_ar) 
+                  {
+                      foreach($comp_ar as $va)
+                         $query->orwhere('company', '=', $va);
+                      })->where(function ($query) use ($loc_ar) 
+                             {
+                                foreach($loc_ar as $va)
+                                   $query->orwhere('location', '=', $va);
+                              })->where('active','1')->where('deadline','>=',now())->where('start_date','<=',now())->orderByRaw('start_date DESC')
+                            ->get(),200);
+          }
+          
+  
+      }
 
     public function filterActiveTender($id_major)
     {
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -37,13 +37,13 @@ Route::delete('major/{id}', 'Major\MajorController@majorDelete');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-  
+});
   Route::namespace('Tender')->group(function(){
     Route::get('Tender/get','TenderController@getActiveTenders'); // for get all tenders 
     Route::get('Tender/get/{id}','TenderController@getTenderById'); //for get tender by its id 
     Route::get('Tender/major','TenderController@getTenderMajor'); // for get all major and its count in tenders 
 
-    //Route::get('Tender/filters','TenderController@filterAllActiveTender');
+    Route::get('Tender/filters','TenderController@filterAllActiveTender');
     //Route::get('Tender/filter/{filters?}','TenderController@filterActiveTender');
 
 });
@@ -51,4 +51,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::apiResource('major', 'Major\MajorController');
 Route::post('login', 'login\loginController@login');
 Route::get('checklogin', 'login\loginController@checklogin');
-
