adc3b87c78df6d6bb5907e021125850cf0c38f44
HaifaaNabeel <hauof53@gmail.com>
2021-01-01 17:54:15 +0300

    contact us api with worktime email

---
+++ backend/resources/views/email/ContactMail.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact message </title>
+</head>
+<body>
+    <h1>Contact message </h1>
+    <p>Name:{{$data['name']}}</p>
+    <p>Message:{{$data['message']}}</p>
+    <p>Email:{{$data['Email']}}</p>
+</body>
+</html>
